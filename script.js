6842d7cec7d35af54720aea447a7f76ba4c2161b
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-28 16:44:48 +0200

    API key setup

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+// Unsplash API
+const count = 10;
+const apiKey = "617037";
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
