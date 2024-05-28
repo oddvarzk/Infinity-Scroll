b0c0f9e12d18261d52d2be292770cdf1ae612457
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-29 00:54:44 +0200

    hardcoded img deleted from HTML, replaced with API imgs

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
 const apiKey = "CSE-u2QWDi9En_pIEFlwdzVnbIuGVpjtLqpgnHET2wA";
-
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create Elements for Links and Photos, Add to Dom
+function displayPhotos() {
+  //Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+    // Create <img> for photo
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    // Put <img> inside <a>, then put both inside imageContainer
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 // Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
-    //Catch error here
+    console.error("Error fetching photos:", error); // Catch and log error here
   }
 }
+
+//On Load
+getPhotos();
 
 //On Load
 getPhotos();
