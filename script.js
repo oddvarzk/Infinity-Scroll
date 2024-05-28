b3dca1600783e60be9d167f0d69bc659beff70c6
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-28 16:51:36 +0200

    API key plus fetch of items complete

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Unsplash API
 const count = 10;
-const apiKey = "617037";
+const apiKey = "CSE-u2QWDi9En_pIEFlwdzVnbIuGVpjtLqpgnHET2wA";
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    //Catch error here
+  }
+}
+
+//On Load
+getPhotos();
